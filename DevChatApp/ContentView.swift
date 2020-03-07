163e45a978c87067f538fc39c9d1edccb35be290
mkohen <email>
2020-03-07 09:41:36 +0200

    Initial Commit

---
+++ DevChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DevChatApp
+//
+//  Created by Mila Rabuchin on 07/03/2020.
+//  Copyright © 2020 Mila Rabuchin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
